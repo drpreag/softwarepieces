9720f16651337edae989fa176f025f847b3ceaa7
Predrag Vlajkovic drPreAG <predrag.vlajkovic@gmail.com>
2017-12-13 11:40:09 +0100

    Few blades fix

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -10,17 +10,27 @@ about us
     <div class="container">
         <h3>About us</h3>
 
-        <div class="well" align="center">
-            ...
-            to add text here
-            ...
-            <br>
-            <br>            
-            Copyright <b>Predrag Vlajkovic drPreAG</b><br>
-            <br>
-            <br>
-            2017<br>
-            All right reserved<br>
+        <div class="well" align="left">
+            <p>
+                SoftwarePieces was started as my personal project and I created it to achieve a few goals. 
+                <br><br>
+                First was to bring all relevant open source world news and informations at single place, so people who think similar then I do can have easy way to get and read them.
+                <br>
+                Also I always wanted to have kind of a blog, where my friends and myself can share our sight on open source stuff.
+                <br><br> 
+                But, during a time project has been growing up, needed some expertises that I don't have, and it was impossible to me to continye all by myself. So I invited a few friend to join me and do here what they are best in. Here are listed a contributions:
+                <br><br>
+                ... add text here ...
+                <br><br>
+                If you are open source fan I hope that you will enjoy this app. Also, if you find yourself as expert in any open source area and you don't have your personal blog yet, I urge you to join a team and start writing for SoftwarePieces.<br><br>
+                Yours, drPreAG.
+            </p>
+            <br>          
+            <p align="center">  
+                Copyright <b>Predrag Vlajkovic drPreAG</b><br>
+                2016-2017<br>
+                All right reserved<br>
+            </p>
         </div>
     </div>
 @endsection
--- resources/views/pages/licence.blade.php
+++ resources/views/pages/licence.blade.php
@@ -8,19 +8,23 @@ licence, terms of use
 
 @section('content')
     <div class="container">
-        <h3>Licence information</h3>
+        <h3>Licence information and teamrs of use</h3>
 
-        <div class="well" align="center">
-            ...
-            to add text here
-            ...
-            <br>
-            <br>
-            Copyright <b>Predrag Vlajkovic drPreAG</b><br>
-            <br>
-            <br>
-            2017<br>
-            All right reserved<br>
+        <div class="well" align="left">
+            <p>
+                SoftwarePieces is a project about OpenSource, and consists of two main parts, News and Blog. 
+                <br><br>
+                Blog part can be used at your own will, because we love Open Source our Blog is also Open Source. 
+                When you are sharing / resharing blog article please put a backlink URL to our original article.
+                <br><br>
+                To use articles in our News section, where we are resharing other people original news, please contact original publisher, we can not give permition to share / reshare, bacause we don't own rights on that articles. We only have right to share on our News section.
+            </p>
+            <br>            
+            <p align="center">
+                Copyright <b>Predrag Vlajkovic drPreAG</b><br>
+                2016-2017<br>
+                All right reserved<br>
+            </p>
         </div>
     </div>
 @endsection
